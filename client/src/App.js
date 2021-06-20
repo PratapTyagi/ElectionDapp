87e0959a1c373f395e96dfc3f36fb626780dc633
Pratap Tyagi <tyagipratap111@gmail.com>
2021-06-20 21:32:34 +0530

    Data received from blockchain to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,47 @@
+import { useEffect } from "react";
 import "./App.css";
+import Electionabi from "./contracts/Election.json";
+import Web3 from "web3";
 
 function App() {
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+
+    const accounts = web3.eth.getAccounts();
+    const account = accounts[0];
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = Electionabi.networks[networkId];
+
+    if (networkData) {
+      const election = new web3.eth.Contract(
+        Electionabi.abi,
+        networkData.address
+      );
+      console.log(election);
+    } else {
+      window.alert("Smart contract not deployed on current network");
+    }
+  };
+
   return (
     <div className="app">
       <h1>Starter</h1>
